b2029e11a56fe7ce236c57f722a265c8d8ffe20b
BigDataBohdan <bagdankvetka@gmail.com>
2023-04-04 13:02:02 +0200

    imgauto

---
+++ imageAuto/imageprj.py
@@ -0,0 +1,17 @@
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = './imgs'
+pathOut = '/editedImg'
+
+for filename in os.listdir(path):
+    img = Image.open(f"{path}/{filename}")
+    
+    #edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(-90)
+    edit = img.filter(ImageFilter.SHARPEN).convert('L').rotate(0)
+    factor = 2.5
+    enchancer = ImageEnhance.Contrast(edit)
+    edit = enchancer.enhance(factor)
+    clean_name = os.path.splitext(filename)[0]
+    
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
